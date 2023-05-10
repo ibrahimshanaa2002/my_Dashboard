9b47d55549527e2e872997225615524748ece263
Mahmoud Abdulal <mahmoud.abdulal@hotmail.com>
2023-05-10 12:48:03 +0300

    changes in bar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {UserData} from "./model/userData.interface";
 import {barChartAxisData} from "./model/barChartAxisData.interface";
+import {Data} from "./model/data.interface";
 
 @Component({
   selector: 'app-root',
@@ -9,44 +10,25 @@ import {barChartAxisData} from "./model/barChartAxisData.interface";
 })
 export class AppComponent {
   title = 'sprinters';
-  userData: UserData ;
+  userData: UserData;
   MPRUserData: Array<UserData>;
 
   constructor() {
-    let xy = new Array<barChartAxisData>;
-    let data = new Map<string, barChartAxisData[]>;
-    xy.push(new barChartAxisData("x1", 20));
-    xy.push(new barChartAxisData("x2", 20));
-    xy.push(new barChartAxisData("x3", 20));
-    xy.push(new barChartAxisData("x4", 20));
-    xy.push(new barChartAxisData("x5", 20));
-    xy.push(new barChartAxisData("x6", 20));
-    xy.push(new barChartAxisData("x7", 20));
-    xy.push(new barChartAxisData("x8", 20));
-    xy.push(new barChartAxisData("x9", 20));
-    xy.push(new barChartAxisData("x10", 20));
-    // xy.push(new barChartAxisData("x11", 20));
+    let data = new Map<string, Data[]>();
+    let x : Data[] = new Array<Data>();
+    x.push(new Data("iponaccount", 25));
+    x.push(new Data("iponapp", 25));
+    x.push(new Data("appsecured", 25));
+    x.push(new Data("appexecuted", 25));
+    let y : Data[] = new Array<Data>();
+    y.push(new Data("iponaccount", 20));
+    y.push(new Data("iponapp", 20));
+    y.push(new Data("appsecured", 20));
+    y.push(new Data("appexecuted", 20));
+    data.set("ahmad",x);
+    data.set("sami",y);
+    this.userData = {data}
 
-
-    let xyz = new Array<barChartAxisData>;
-    xyz.push(new barChartAxisData("x1", 20));
-    xyz.push(new barChartAxisData("x2", 20));
-    xyz.push(new barChartAxisData("x3", 20));
-    xyz.push(new barChartAxisData("x4", 20));
-    xyz.push(new barChartAxisData("x5", 20));
-
-
-    xyz.push(new barChartAxisData("x6", 20));
-    xyz.push(new barChartAxisData("x7", 20));
-    xyz.push(new barChartAxisData("x8", 20));
-    xyz.push(new barChartAxisData("x9", 20));
-    xyz.push(new barChartAxisData("x10", 20));
-    xyz.push(new barChartAxisData("x11", 20));
-    data.set("xy", xy);
-    data.set("xyz", xyz);
-    this.userData = {
-      data: data
-    };
   }
 
   changeWidth(state: boolean){
